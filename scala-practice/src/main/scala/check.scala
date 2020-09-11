e0820078cf5e18de7e258b123cdab9630e839f2b
potyama <kamikaze117117@gmail.com>
2020-09-12 00:00:04 +0900

    for, matchパターンの追加

--- scala-practice/src/main/scala/check.scala
+++ scala-practice/src/main/scala/check.scala
@@ -17,12 +17,33 @@ object Main extends App{
       i += 1
     } while (i < 10)
   }
-  println(loopFrom0to9())
+  loopFrom0to9()
+
   //for
   def triangle(): Unit = {
     for (a <- 1 to 1000; b <- 1 to 1000; c <- 1 to 1000 if a * a == b * b + c * c) {
       println ((a, b, c) )
     }
   }
-  
+  //triangle()
+
+  //match
+  var result = "ryunen"
+
+  result match {
+    case "ryunen" => println("kasu")
+    case "sinkyu" => println("pro")
+  }
+
+  var list = List("x", "y", "z")
+
+  list match {
+    case List("x", b, c) =>
+      println("y = " + b)
+      println("z = " + c)
+    case _ =>
+      println("nothing")
+  }
+
+
 }
